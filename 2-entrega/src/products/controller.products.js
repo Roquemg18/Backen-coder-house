44b0217ca47ce5626492d36f38906794bb439351
Roquemg <molinaroque871@gmail.com>
2023-04-20 08:06:01 -0300

    products terminado

--- 2-entrega/src/products/controller.products.js
+++ 2-entrega/src/products/controller.products.js
@@ -14,70 +14,60 @@ const router = Router();
 
 
 router.get('/', async (req, res) => {
-    try {
-      let limit = req.query.limit || 10;
-      let page = req.query.page || 1;
-      let sort = req.query.sort || '';
-      let query = req.query.query || '';
-      let category = req.query.category || '';
-      let availability = req.query.availability || '';
-  
-      let filters = {};
-  
-      if (query !== '') {
-        filters = { name: { $regex: query, $options: 'i' } };
-      }
-  
-      if (category !== '') {
-        filters = { ...filters, category: category };
-      }
-  
-      if (availability !== '') {
-        filters = { ...filters, available: availability };
-      }
-  
-      let sortOrder = {};
-      if (sort !== '') {
-        sortOrder = { price: sort === 'asc' ? 1 : -1 };
-      }
-  
-      const options = {
-        page: parseInt(page),
-        limit: parseInt(limit),
-        sort: sortOrder,
-      };
-      
-      const products = await Product.paginate(filters, options);
-      
-      const infoProductos = products.docs
-      
-      const totalPages = products.totalPages;
-      const prevPage = products.prevPage;
-      const nextPage = products.nextPage;
-      const currentPage = products.page;
-      const hasPrevPage = products.hasPrevPage;
-      const hasNextPage = products.hasNextPage;
-      const prevLink = hasPrevPage ? `http://${req.headers.host}${req.baseUrl}?page=${prevPage}&limit=${limit}` : null;
-      const nextLink = hasNextPage ? `http://${req.headers.host}${req.baseUrl}?page=${nextPage}&limit=${limit}` : null;
-      
-      const infoProducts = {
-        status: 'success',
-        totalPages: totalPages,
-        prevPage: prevPage,
-        nextPage: nextPage,
-        page: currentPage,
-        hasPrevPage: hasPrevPage,
-        hasNextPage: hasNextPage,
-        prevLink: prevLink,
-        nextLink: nextLink,
-        products: products.docs}
+     const { limit = 10, page = 1, sort, query, category, availability } = req.query;
 
-      res.render("products.handlebars",{infoProductos})
-      
-    } catch (error) {
-      return res.status(400).render("error.handlebars", { error: error.message });
+  const options = {
+    limit: parseInt(limit),
+    page: parseInt(page),
+    sort: sort && { price: sort === 'asc' ? 1 : -1 },
+    customLabels: {
+      docs: 'payload',
+      totalDocs: 'totalProducts',
+      totalPages: 'totalPages'
     }
-  });
+  };
+
+  const filter = {};
+
+  if (category) {
+    filter.category = category;
+  }
+
+  if (availability) {
+    filter.availability = availability;
+  }
+
+  if (query) {
+    filter.$text = { $search: query };
+  }
+
+  try {
+    const products = await Product.paginate(filter, options);
+    
+    const payload = products.payload.map((product) => ({
+      id: product._id,
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      stock: product.stock 
+    }));
+
+    const totalPages = products.totalPages;
+    const prevPage = products.prevPage || 1;
+    const nextPage = products.nextPage || totalPages;
+    const currentPage = products.page;
+    const hasPrevPage = products.hasPrevPage;
+    const hasNextPage = products.hasNextPage;
+    const prevLink = hasPrevPage && `/products?page=${prevPage}&limit=${limit}&sort=${sort}&query=${query}&category=${category}&availability=${availability}`;
+    const nextLink = hasNextPage && `/products?page=${nextPage}&limit=${limit}&sort=${sort}&query=${query}&category=${category}&availability=${availability}`;
+
+    res.render('products.handlebars',{payload,totalPages,prevPage,nextPage,currentPage,prevLink,nextLink})
+    
+  } catch (error) {
+    next(error);
+  }
+})
+
 
 router.get('/loadData',async (req, res) => {
     try {
